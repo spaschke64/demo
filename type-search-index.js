f06496bb2640871c1ed37f0df750e64610d25302
Steve Paschke <spaschke@mac.com>
2023-08-25 11:09:13 -0600

    filtered and converted data to json.  imaging isn't happening yet

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.demo","l":"CdfController"},{"p":"com.example.demo","l":"CdfService"},{"p":"com.example.demo","l":"DemoApplication"}];updateSearchResults();
